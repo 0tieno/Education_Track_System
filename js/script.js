24f14f998048681905577c9bd2777657b3b70153
Otieno <ronvi035@gmail.com>
2023-12-20 18:15:46 +0300

    changed the variable declaration

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function() {
-    var navigation = document.getElementById('navbar');
+    let navigation = document.getElementById('navbar');
     if (window.scrollY > 500) { // Adjust the scroll threshold as needed
         navigation.classList.add('fixed-nav');
     } else {
